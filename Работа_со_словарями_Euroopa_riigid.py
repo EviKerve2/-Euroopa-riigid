6d665d07b94f2151730962851cb561bf8f9760fa
opilane <opilane@HP-CZC6297B82>
2021-12-08 12:26:22 +0200

    Работа со словарями "Euroopa riigid"

--- Работа_со_словарями_Euroopa_riigid.py
+++ Работа_со_словарями_Euroopa_riigid.py
@@ -1,15 +1,8 @@
-Capitals={} 
-Capitals = dict() 
-Capitals['Russia'] = 'Moscow'
-Capitals['Ukraine'] = 'Kiev'
-Capitals['USA'] = 'Washington'
-Countries = ['Russia', 'France', 'USA', 'Russia'] 
-for country in Countries:
- if country in Capitals:
-    print('Столица страны ' + country + ': ' + Capitals[country])
- else:
-    print('В базе нет страны c названием ' + country)
-def 
-
-while 1:
-    print("\n1 - Ввести город или страну. \n2 - Добавить в список город или страну. \n3 - Исравить ошибку в списке. \n4 - Проверь себя.")
+sonastik={}
+file=open("riigid_pealinnad.txt", 'r')
+for line in file:
+    k, v=line.ctrip().strip('-')
+    sonastik[k.strip()]=v.strip()
+    sonastik[k.strip()]=v.strip()
+    
+print(sonastik)
